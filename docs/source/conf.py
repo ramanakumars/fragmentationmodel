b65ac0f4ec2c7177337c18b6b217287210c7daad
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2025-07-11 11:03:01 -0700

    updating docs to pull from examples/ folder

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
+import shutil
 import sys
+
 sys.path.insert(0, os.path.abspath('../..'))
 
 project = 'Fragmentation Model'
@@ -27,7 +29,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.githubpages',
     'myst_parser',
-    'nbsphinx'
+    'nbsphinx',
 ]
 
 templates_path = ['_templates']
@@ -41,5 +43,23 @@ html_static_path = ['_static']
 html_theme_options = {
     "repository_url": "https://github.com/ramanakumars/fragmentationmodel",
     "repository_branch": "main",
-    "use_repository_button": True
+    "use_repository_button": True,
 }
+
+
+# -- copy over the notebook examples
+def all_but_ipynb(dir, contents):
+    result = []
+    for c in contents:
+        if os.path.isfile(os.path.join(dir, c)) and (not c.endswith(".ipynb")):
+            result += [c]
+    return result
+
+
+project_root = "../../"
+shutil.rmtree(os.path.join(project_root, "docs/source/notebooks"), ignore_errors=True)
+shutil.copytree(
+    os.path.join(project_root, "examples"),
+    os.path.join(project_root, "docs/source/notebooks"),
+    ignore=all_but_ipynb,
+)
